70d20495471dd38b756f659366c91cbd9859102e
Alexander <Alexyammine94@gmail.com>
2020-04-10 12:27:26 -0400

    Added getElement, updateElementy and removeElement

--- list/list.go
+++ list/list.go
@@ -1,5 +1,9 @@
 package list
 
+import (
+	"errors"
+)
+
 type node struct {
 	next *node
 	data interface{}
@@ -7,10 +11,105 @@ type node struct {
 
 type List struct {
 	// Attributes
-	fist *node
-	last *node
+	fist  *node
+	last  *node
+	count int
 }
 
 func (l *List) IsEmpty() bool {
 	return l.fist == nil
+}
+
+func (l *List) ItemsCount() int {
+	return l.count
+}
+
+func (l *List) AddElement(data interface{}) error {
+	if l.fist == nil {
+		temp := node{next: nil, data: data}
+		l.fist = &temp
+		l.last = l.fist
+		l.count++
+	} else {
+		temp := node{next: nil, data: data}
+		l.last.next = &temp
+		l.last = &temp
+		l.count++
+	}
+	return nil
+}
+
+func (l *List) GetElement(item int) (interface{}, error) {
+	if item > l.count || item < 0 {
+		return nil, errors.New("Invalid item number")
+	}
+	i := 0
+	var data interface{}
+	temp := l.fist
+	for {
+		if i == item {
+			data = temp.data
+			break
+		}
+		temp = temp.next
+		i++
+	}
+	return data, nil
+}
+
+func (l *List) UpdateElement(item int, newData interface{}) error {
+	if item > l.count || item < 0 {
+		return errors.New("Invalid item number")
+	}
+	i := 0
+	temp := l.fist
+	for {
+		if i == item {
+			temp.data = newData
+			break
+		}
+		temp = temp.next
+		i++
+	}
+	return nil
+}
+
+func (l *List) RemoveElement(item int) error {
+	if item > l.count || item < 0 {
+		return errors.New("Invalid item number")
+	}
+	// Remove first element
+	if item == 0 {
+		temp := l.fist
+		l.fist = l.fist.next
+		temp.next = nil
+		temp.data = nil
+		l.count--
+
+		return nil
+	}
+	// Remove element
+	i := 0
+	temp := l.fist
+	for {
+		temp0 := temp.next
+		i++
+		if i == item {
+			if temp0.next == nil {
+				// temp0 is the las item
+				l.last = temp
+				l.last.next = nil
+				temp0.data = nil
+			} else {
+				// temp0 is not the las item
+				temp.next = temp0.next
+				temp0.next = nil
+				temp0.data = nil
+			}
+			l.count--
+			break
+		}
+		temp = temp.next
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -8,5 +8,23 @@ import (
 
 func main() {
 	var a list.List
-	fmt.Print(a.IsEmpty())
+	a.AddElement(0)
+	a.AddElement(5)
+	a.AddElement(10)
+	a.AddElement(15)
+	a.AddElement(20)
+	a.UpdateElement(2, 100)
+	//data, _ := a.GetElement(2)
+	//fmt.Print(data, "\n")
+	_ = a.RemoveElement(0)
+	//data0, _ := a.GetElement(2)
+	//fmt.Print(data0, "\n")
+	_ = a.RemoveElement(2)
+	//data1, _ := a.GetElement(2)
+	//fmt.Print(data1, "\n")
+	N := a.ItemsCount()
+	for i := 0; i < N; i++ {
+		element, _ := a.GetElement(i)
+		fmt.Print(element, "\n")
+	}
 }
